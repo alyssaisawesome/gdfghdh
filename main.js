82801440a365a33d559fc36061c58424c87dbd92
alyssaisawesome <86136406+alyssaisawesome@users.noreply.github.com>
2022-07-02 11:03:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+song1="";
+song2="";
+rightwristX=0;
+rightwristY=0;
+leftwristX=0;
+leftwristY=0;
+leftWristScore=0;
+rightWristScore=0;
+songStatus="";
+songStatus2="";
+
+function preload() {
+  song1=loadSound('music.mp3');
+  song2=loadSound('music2.mp3');
+}
+
+function setup() {
+  canvas = createCanvas(500,500);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+
+  posenet = ml5.poseNet(video,modelLoaded);
+  posenet.on('pose',gotPoses);
+  }
+
+  function modelLoaded() {
+    console.log('model loaded');
+  }
+
+  function gotPoses(results) {
+    if (results.length>0) {
+  console.log(results);
+  leftWristScore=results[0].pose.keypoints[9].score;
+  rightWristScore=results[0].pose.keypoints[10].score;
+  rightWristX=results[0].pose.rightWrist.x;
+  rightWristY=results[0].pose.rightWrist.y;
+  leftWristX=results[0].pose.leftWrist.x;
+  leftWristY=results[0].pose.leftWrist.y;
+  console.log('right wrist x = ' + rightWristX + ' right wrist y = ' + rightWristY + ' left wrist x = ' + leftWristX + ' left wrist y = ' + leftWristY);
+    }
+  }
+
+  function draw() {
+    image(video,0,0,600,500);
+    fill('red');
+    stroke('red');
+    songStatus=song1.isPlaying();
+    songStatus2=song2.isPlaying();
+    if (leftWristScore>0.2) {
+      circle(leftWristX,leftWristY,20);
+      song2.stop();
+      if (songStatus==false) {
+        song1.play();
+        document.getElementById('song').innerHTML="Song Name: whatever the song name is - song 1"
+      }
+    }
+
+    if (rightWristScore>0.2) {
+      fill('blue');
+      stroke('blue');
+      circle(rightWristX,rightWristY,20);
+      song1.stop();
+      if (songStatus==false) {
+        song2.play();
+        document.getElementById('song').innerHTML="Song Name: whatever the song name is - song 2"
+      }
+    }
+  }
